1fe429209547361bedb7760bb48005a568e40059
mattak <mattak.me@gmail.com>
2015-01-22 11:44:09 +0900

    parse url Link modified

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 $(function() {
   chrome.tabs.getSelected(null, function(tab){
     var tablink = tab.url;
-    var regexp = new RegExp("/file/d/([^\\/]+)/view");
+    var regexp = new RegExp("/file/d/([^\\/]+)/(view|edit)");
     var match = regexp.exec(tablink);
 
-    if (match.length > 1) {
+    if (match && match.length > 1) {
       var drivelink = match[1];
       var exportlink = "https://drive.google.com/uc?export=view&id=" + drivelink;
       $("#text").text(exportlink);
