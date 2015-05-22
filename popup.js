7d739956d4fadf1e22b41f2550190f1c3dba82e2
mattak <mattak.me@gmail.com>
2015-05-22 19:32:13 +0900

    add authuser & jira link

--- popup.js
+++ popup.js
@@ -6,11 +6,14 @@ $(function() {
 
     if (match && match.length > 1) {
       var drivelink = match[1];
-      var exportlink = "https://drive.google.com/uc?id=" + drivelink + "&ext=.png";
+      var exportlink = "https://drive.google.com/uc?id=" + drivelink + "&authuser=0&ext=.png";
       $("#text_original").text(exportlink);
 
       $("#text_hatena").text("[" + exportlink + ":image:w600]");
       $("#text_hatena").select();
+
+      $("#text_jira").text("!" + exportlink + "|width=600!");
+      $("#text_jira").select();
 
       $("#link").attr("href", exportlink);
       $("#link").text(exportlink);
